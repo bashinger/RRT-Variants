8058184758d05fc6765ccdb9c2f9d86b6b0204c0
redEye <nsagarreddy3@gmail.com>
2024-03-24 20:05:21 +0530

    Add: Module Q-RRT*

--- main.py
+++ main.py
@@ -3,22 +3,17 @@ from map_layouts import layout_simple_cross, layout_maze, layout_urban
 from rrt import RRT
 from rrt_star import RRT_Star
 from dt_rrt_star import DT_RRT_Star
+from q_rrt_star import Q_RRT_Star
 
 if __name__ == "__main__":
     env = MapEnvironment(layout=layout_maze())
     # env.preview_layout()
 
-    ## RRT
-    # rrt = RRT(env)
-    # final_node, path = rrt.find_path()
-    # env.visualize_path(rrt.nodes, path)
+    # Choose the RRT Variant to use
+    variant = RRT(env)
+    # variant = RRT_Star(env)
+    # variant = Q_RRT_Star(env)
+    # variant = DT_RRT_Star(env)
 
-    ## RRT*
-    # rrt_star = RRT_Star(env)
-    # final_node, path = rrt_star.find_path()
-    # env.visualize_path(rrt_star.nodes, path)
-
-    # DT-RRT*
-    dt_rrt_star = DT_RRT_Star(env)
-    final_node, path = dt_rrt_star.find_path()
-    env.visualize_path(dt_rrt_star.nodes, path)
+    final_node, path = variant.find_path()
+    env.visualize_path(variant.nodes, path)
