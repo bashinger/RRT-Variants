21a4ce0f2ffee4106480aab4b4d7230c2bfc508c
mavenor <69630953+mavenor@users.noreply.github.com>
2024-05-09 12:00:13 +0530

    demo convenience

--- main.py
+++ main.py
@@ -8,21 +8,39 @@ from q_rrt_star import Q_RRT_Star
 from lazy_dt_rrt_star import Lazy_DT_RRT_Star
 
 if __name__ == "__main__":
-    env = Map(LayoutCross())
-    # env.preview_layout()
+
+    # CROSS LAYOUT
+    # env = Map(LayoutCross())
+    # planner = DT_RRT_Star(env)
+    # planner = Lazy_DT_RRT_Star(env)
+
+    # MAZE LAYOUT
+    # env = Map(LayoutMaze())
+    # planner = DT_RRT_Star(env)
+    # planner = Lazy_DT_RRT_Star(env)
+
+    # ALLEY LAYOUT
+    # env = Map(LayoutAlley())
+    # planner = DT_RRT_Star(env)
+    # planner = Lazy_DT_RRT_Star(env)
+
+    # SPACE LAYOUT
+    # env = Map(LayoutSpace())
+    # planner = DT_RRT_Star(env)
+    # planner = Lazy_DT_RRT_Star(env)
+
+    renderer = DynamicVisualiser(1, env, planner)
+    # renderer = Visualiser(env)
 
     # Choose the RRT Variant to use
     # variant = RRT(env)
     # variant = RRT_Star(env)
     # variant = Q_RRT_Star(env)
-    # variant = DT_RRT_Star(env)
-    # variant = Lazy_DT_RRT_Star(env)
-
 
     # (a)
-    base_planner = RRT(env)
+    # base_planner = RRT(env)
     # planner = DT_RRT_Star.from_rrt(base_planner)
-    planner = Lazy_DT_RRT_Star.from_rrt(base_planner)
+    # planner = Lazy_DT_RRT_Star.from_rrt(base_planner)
 
     # OR
 
@@ -36,5 +54,3 @@ if __name__ == "__main__":
 
     # # planner = DT_RRT_Star(env, rrt_path=reference_path)
     # planner = Lazy_DT_RRT_Star(env, rrt_path=reference_path)
-
-    renderer = DynamicVisualiser(1, env, planner)
